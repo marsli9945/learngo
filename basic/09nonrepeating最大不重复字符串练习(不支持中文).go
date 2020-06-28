de730ad0273a4913e40968ff83946af5fb694a4e
lilei <li8369945>
2020-06-28 09:51:49 +0800

    基础部分完成

---
+++ basic/09nonrepeating最大不重复字符串练习(不支持中文).go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func lengthNonRepeatingSubStr(s string) int {
+	lastOccurred := make(map[byte]int)
+	start := 0
+	maxLength := 0
+
+	for i, ch := range []byte(s) {
+		if lastI, ok := lastOccurred[ch]; ok && lastI >= start {
+			start = lastI + 1
+		}
+		if i-start+1 > maxLength {
+			maxLength = i - start + 1
+		}
+		lastOccurred[ch] = i
+	}
+	return maxLength
+}
+
+func main() {
+	fmt.Println(lengthNonRepeatingSubStr("abcabcbb"))
+	fmt.Println(lengthNonRepeatingSubStr("bbbbb"))
+	fmt.Println(lengthNonRepeatingSubStr("pwwkew"))
+	fmt.Println(lengthNonRepeatingSubStr(""))
+	fmt.Println(lengthNonRepeatingSubStr("b"))
+	fmt.Println(lengthNonRepeatingSubStr("abcdef"))
+	fmt.Println(lengthNonRepeatingSubStr("这里是幕客网"))
+	fmt.Println(lengthNonRepeatingSubStr("一二三二一"))
+}
